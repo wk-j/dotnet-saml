b05abcfa849840f7e09228308b2c5b773e59f0a2
wk <somnuk.wk@gmail.com>
2020-04-21 21:46:25 +0700

    Docker

--- src/MyWeb/Startup.cs
+++ src/MyWeb/Startup.cs
@@ -22,13 +22,11 @@ namespace MyWeb
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +34,7 @@ namespace MyWeb
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
